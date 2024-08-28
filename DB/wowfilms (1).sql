53ef7cc07ce7633fa6d5113e1860db0ba6bb59f1
Tharindu2005V# <nipuntharindu2005@gmail.com>
2024-08-29 02:40:34 +0530

    m

--- DB/wowfilms (1).sql
+++ DB/wowfilms (1).sql
@@ -47,7 +47,7 @@ CREATE TABLE `subtittletable` (
   `name` varchar(255) NOT NULL,
   `fileUrl` varchar(255) DEFAULT NULL,
   `filmId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
